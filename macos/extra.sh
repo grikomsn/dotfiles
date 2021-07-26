b0b9d4e6784222905e57589040c67c3b6a07f6b8
Griko Nibras <git@griko.id>
2021-07-26 15:53:30 +0700

    feat(macos): add virtualenv setup

--- macos/extra.sh
+++ macos/extra.sh
@@ -103,4 +103,7 @@ npm -g i npm yarn && yarn_update_globals
 curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim \
   --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+# prep virtualenv
+mkdir -p ~/.virtualenvs && virtualenv ~/.virtualenvs/main
+
 echo "Done! ✨"
